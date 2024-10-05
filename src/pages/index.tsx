cf8bbe15cba47130963b89e0c2938aeec5a75821
serabariq <muhammad.bariq@sera.astra.co.id>
2024-10-05 18:23:17 +0700

    [ADD] login page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,115 +1,104 @@
 import Image from "next/image";
-import localFont from "next/font/local";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import Link from "next/link";
+import React, { useState } from "react";
+import TiktokLogo from "../../public/tik-tok.png";
+import LogoTiktok from "@/components/LogoTiktok";
+import IconQuestionMark from "@/components/IconQuestionMark";
+import IconChevLeft from "@/components/IconChevLeft";
 
 export default function Home() {
-  return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/pages/index.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+	const [formInput, setFormInput] = useState({
+		username: "",
+		otp: "",
+	});
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+	return (
+		<div className="flex flex-col h-screen">
+			<nav className="py-4 px-3 flex justify-between">
+				<Link href="/">
+					<LogoTiktok />
+				</Link>
+
+				<Link href="/help" className="flex items-center gap-1">
+					<IconQuestionMark />
+					<span>Feedback and help</span>
+				</Link>
+			</nav>
+			<section>
+				<div className="mx-auto lg:w-96 mt-12">
+					<h1 className="font-bold text-center text-3xl">Login</h1>
+					<form action="" method="POST" className="mt-5">
+						<div className="flex items-center justify-between mb-1">
+							<span className="text-gray-800 font-medium">Phone</span>
+							<span className="text-gray-600 font-medium text-xs">Log in with email or username</span>
+						</div>
+
+						<div className="bg-gray-100 rounded-lg h-10 py-1 border border-gray-300 flex items-center mb-3">
+							<select name="" id="" className="bg-transparent p-2">
+								<option value="" selected>
+									ID +62
+								</option>
+							</select>
+							<span className="w-[1px] h-full bg-gray-300 mx-2 flex" />
+							<input
+								type="tel"
+								name="username"
+								placeholder="Phone number"
+								className="bg-transparent flex-grow focus:outline-none"
+								onChange={e => {
+									setFormInput({
+										username: e.target.value,
+										otp: formInput.otp,
+									});
+								}}
+							/>
+						</div>
+
+						<div className="bg-gray-100 rounded-lg h-12 border border-gray-300 flex items-center mb-3">
+							<input
+								type="text"
+								maxLength={6}
+								placeholder="Enter 6-digit code"
+								className="flex-grow bg-transparent px-3 focus:outline-none"
+							/>
+							<span className="w-[1px] h-full bg-gray-300 ms-2 flex" />
+
+							<button
+								className="bg-transparent border-none disabled:text-gray-400 font-medium h-full px-4 disabled:cursor-not-allowed"
+								disabled={formInput.username.length === 0}
+							>
+								Send code
+							</button>
+						</div>
+
+						<Link href="https://www.tiktok.com/login/phone-or-email/phone-password" className="text-gray-800 text-sm">
+							Log in with password
+						</Link>
+
+						<div className="mt-4">
+							<button className="w-full p-4 rounded-lg font-bold bg-[#fe2c55] text-white">Log in</button>
+							<button className="w-full p-4 rounded-lg bg-transparent flex items-center gap-1 justify-center mt-8">
+								<IconChevLeft />
+								<span>Go back</span>
+							</button>
+						</div>
+					</form>
+				</div>
+			</section>
+			<div className="flex items-center gap-2 mt-auto py-4 justify-center border-t border-gray-100">
+				<span className="text-gray-800">Don’t have an account?</span>
+				<Link href="/" className="text-[#fe2c55]">
+					Sign up
+				</Link>
+			</div>
+			<footer className="bg-black text-white py-7 px-32">
+				<div className="container mx-auto flex items-center justify-between">
+					<select name="" id="" className="lg:min-w-40 bg-transparent border border-gray-500 focus:outline-none p-2">
+						<option value="english">English</option>
+					</select>
+					<small className="text-gray-400 text-base">&copy; {new Date().getFullYear()} TikTok</small>
+				</div>
+			</footer>
+		</div>
+	);
 }
